03bc7b28bf91819a089e32ccc1e372c8b51b465f
Michael Rhodes <mrhodes@mediapolis.com>
2019-12-25 00:16:05 -0400

    fix async/await on recursive delete

--- modules/digitopia-cms/lib/db-sequelize.js
+++ modules/digitopia-cms/lib/db-sequelize.js
@@ -1,295 +1,283 @@
-const debug = require('debug')('antisocial-db');
-const VError = require('verror').VError;
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-const basename = path.basename(__filename);
-const admin = require("./admin");
-const _ = require('lodash');
-const async = require('async');
+const debug = require('debug')('antisocial-db')
+const VError = require('verror').VError
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const admin = require('./admin')
+const _ = require('lodash')
+const async = require('async')
 
-const EventEmitter = require('events');
+const EventEmitter = require('events')
 
 // instantiate sequelize
 // load models
 // setup hooks to emit emit events for create, delete and update
 
 class dbHandler extends EventEmitter {
+  constructor (app, options) {
+    debug('dbHandler create')
 
-	constructor(app, options) {
-		debug('dbHandler create');
+    super()
+    this.app = app
+    this.options = options.dbOptions
+    this.adminOptions = options.adminOptions
 
-		super();
-		this.app = app;
-		this.options = options.dbOptions;
-		this.adminOptions = options.adminOptions;
+    this.modelDefs = []
 
-		this.modelDefs = [];
+    // the sequelize config file is used for sequelize-cli db:migrate but we don't
+    // keep production credentials there - they are passed into this in 'options'
+    this.sequelize = new Sequelize(options.db, options.user, options.password, options.dbOptions)
 
-		// the sequelize config file is used for sequelize-cli db:migrate but we don't
-		// keep production credentials there - they are passed into this in 'options'
-		this.sequelize = new Sequelize(options.db, options.user, options.password, options.dbOptions);
+    const modelDirs = [path.join(__dirname, '../', 'models')]
+    modelDirs.push(path.join(__dirname, '../../../models'))
 
-		let modelDirs = [path.join(__dirname, '../', 'models')];
-		modelDirs.push(path.join(__dirname, '../../../models'));
+    // load models in models directory (exclude models/index.js)
 
+    modelDirs.map((dir) => {
+      debug('dbHandler loding models in ' + dir)
+      fs
+        .readdirSync(dir)
+        .filter(file => {
+          return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js')
+        })
+        .forEach(file => {
+          const modelDef = this.sequelize.import(path.join(dir, file))
+          this.modelDefs[modelDef.name] = modelDef
+        })
+    })
 
-		// load models in models directory (exclude models/index.js)
+    Object.keys(this.modelDefs).forEach(modelName => {
+      if (this.modelDefs[modelName].associate) {
+        this.modelDefs[modelName].associate(this.modelDefs)
+      }
+    })
 
-		modelDirs.map((dir) => {
-			debug('dbHandler loding models in ' + dir);
-			fs
-				.readdirSync(dir)
-				.filter(file => {
-					return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js');
-				})
-				.forEach(file => {
-					let modelDef = this.sequelize['import'](path.join(dir, file));
-					this.modelDefs[modelDef.name] = modelDef;
-				});
-		});
+    var self = this
 
-		Object.keys(this.modelDefs).forEach(modelName => {
-			if (this.modelDefs[modelName].associate) {
-				this.modelDefs[modelName].associate(this.modelDefs);
-			}
-		});
+    this.sequelize.addHook('afterCreate', (model, options) => {
+      debug('dbHandler emit db-create')
+      self.emit('db-create', model.constructor.name, model)
+    })
 
+    this.sequelize.addHook('beforeDestroy', (model, options) => {
+      debug('dbHandler emit db-delete')
+      self.emit('db-delete', model.constructor.name, model)
+    })
 
-		var self = this;
+    this.sequelize.addHook('afterUpdate', (model, options) => {
+      debug('dbHandler emit db-update')
+      self.emit('db-update', model.constructor.name, model)
+    })
+  }
 
-		this.sequelize.addHook('afterCreate', (model, options) => {
-			debug('dbHandler emit db-create');
-			self.emit('db-create', model.constructor.name, model);
-		});
+  sync (done) {
+    debug('calling sequelize.sync()')
+    this.sequelize.sync().then(() => {
+      // if no user roles create superuser and admin
+      this.getInstances('Role', {}, (err, roles, count) => {
+        if (err) {
+          return done(err)
+        }
+        if (count) {
+          return done()
+        }
+        async.series([
+          (cb) => {
+            this.newInstance('Role', {
+              description: 'superuser'
+            }, cb)
+          }, (cb) => {
+            this.newInstance('Role', {
+              description: 'admin'
+            }, cb)
+          }, (cb) => {
+            this.newInstance('Role', {
+              description: 'owner'
+            }, cb)
+          }
+        ], function (err) {
+          if (err) {
+            debug('error creating default roles', err)
+          }
+          debug('default roles created')
+          done()
+        })
+      })
 
-		this.sequelize.addHook('beforeDestroy', (model, options) => {
-			debug('dbHandler emit db-delete');
-			self.emit('db-delete', model.constructor.name, model);
-		});
+      // now that the db is up we can initialize /admin
+      if (this.adminOptions) {
+        this.app.admin = require('./admin')
+        this.app.admin.mount(this.app, this.adminOptions)
+      }
+    })
+  }
 
-		this.sequelize.addHook('afterUpdate', (model, options) => {
-			debug('dbHandler emit db-update');
-			self.emit('db-update', model.constructor.name, model);
-		});
-	}
+  getModel (modelName) {
+    if (!this.sequelize.models[modelName]) {
+      throw (new Error('db-sequelize getModel model not found: ' + modelName))
+    }
+    return this.sequelize.models[modelName]
+  }
 
-	sync(done) {
-		debug('calling sequelize.sync()');
-		this.sequelize.sync().then(() => {
-			// if no user roles create superuser and admin
-			this.getInstances('Role', {}, (err, roles, count) => {
-				if (count) {
-					return done();
-				}
-				async.series([
-					(cb) => {
-						this.newInstance('Role', {
-							description: 'superuser'
-						}, cb);
-					}, (cb) => {
-						this.newInstance('Role', {
-							description: 'admin'
-						}, cb);
-					}, (cb) => {
-						this.newInstance('Role', {
-							description: 'owner'
-						}, cb);
-					}
-				], function (err) {
-					if (err) {
-						debug('error creating default roles', err);
-					}
-					debug('default roles created');
-					done();
-				});
-			});
+  newInstance (modelName, data, cb) {
+    debug('dbHandler.newInstance %s', modelName)
+    for (const prop in data) {
+      if (!data[prop]) {
+        delete data[prop]
+      }
+    }
+    this.getModel(modelName).create(data)
+      .then(function (instance) {
+        cb(null, instance)
+      })
+      .catch(function (err) {
+        cb(new VError(err, 'newInstance error'))
+      })
+  }
 
-			// now that the db is up we can initialize /admin
-			if (this.adminOptions) {
-				this.app.admin = require("./admin");
-				this.app.admin.mount(this.app, this.adminOptions);
-			}
-		});
-	}
+  getInstance (modelName, id, cb) {
+    this.getModel(modelName).findByPk(id)
+      .then(function (result) {
+        debug('dbHandler.getInstance %s %s found: %j', modelName, id, result)
+        cb(null, result)
+      })
+      .catch(function (err) {
+        cb(new VError(err, 'getInstances error'))
+      })
+  }
 
-	getModel(modelName) {
-		if (!this.sequelize.models[modelName]) {
-			throw (new Error('db-sequelize getModel model not found: ' + modelName))
-		}
-		return this.sequelize.models[modelName];
-	}
+  getInstances (modelName, query, cb) {
+    this.getModel(modelName).findAndCountAll(query)
+      .then(function (result) {
+        debug('dbHandler.getInstances %s %j found: %s', modelName, query, result.rows.length)
 
-	newInstance(modelName, data, cb) {
-		debug('dbHandler.newInstance %s', modelName);
-		for (let prop in data) {
-			if (!data[prop]) {
-				delete data[prop];
-			}
-		}
-		this.getModel(modelName).create(data)
-			.then(function (instance) {
-				cb(null, instance);
-			})
-			.catch(function (err) {
-				cb(new VError(err, 'newInstance error'));
-			});
-	}
+        cb(null, result.rows, result.count)
+      })
+      .catch(function (err) {
+        cb(new VError(err, 'getInstances error'))
+      })
+  }
 
-	getInstance(modelName, id, cb) {
-		this.getModel(modelName).findByPk(id)
-			.then(function (result) {
-				debug('dbHandler.getInstance %s %s found: %j', modelName, id, result);
-				cb(null, result);
-			})
-			.catch(function (err) {
-				cb(new VError(err, 'getInstances error'));
-			});
-	}
+  updateInstance (modelName, id, patch, cb) {
+    debug('dbHandler.updateInstance %s %s %j', modelName, id, patch)
+    this.getModel(modelName).findOne({
+      where: {
+        id: id
+      }
+    })
+      .then(function (instance) {
+        for (const prop in patch) {
+          instance[prop] = patch[prop] ? patch[prop] : null
+        }
+        instance.save()
+          .then(function () {
+            cb(null, instance)
+          })
+          .catch(function (err) {
+            cb(new VError(err, 'updateInstance error'))
+          })
+      })
+      .catch(function (err) {
+        cb(new VError(err, 'updateInstance error ' + modelName + ' id:' + id + ' not found'))
+      })
+  }
 
-	getInstances(modelName, query, cb) {
-		this.getModel(modelName).findAndCountAll(query)
-			.then(function (result) {
-				debug('dbHandler.getInstances %s %j found: %s', modelName, query, result.rows.length);
+  async doDelete (modelName, instance) {
+    let deps
+    try {
+      deps = await admin.getDependants(modelName, instance)
 
-				cb(null, result.rows, result.count);
-			})
-			.catch(function (err) {
-				cb(new VError(err, 'getInstances error'));
-			});
-	}
+      // recursively delete dependant rows
 
-	updateInstance(modelName, id, patch, cb) {
-		debug('dbHandler.updateInstance %s %s %j', modelName, id, patch);
-		this.getModel(modelName).findOne({
-				where: {
-					id: id
-				}
-			})
-			.then(function (instance) {
-				for (let prop in patch) {
-					instance[prop] = patch[prop] ? patch[prop] : null;
-				}
-				instance.save()
-					.then(function () {
-						cb(null, instance);
-					})
-					.catch(function (err) {
-						cb(new VError(err, 'updateInstance error'));
-					});
-			})
-			.catch(function (err) {
-				cb(new VError(err, 'updateInstance error ' + modelName + ' id:' + id + ' not found'));
-			});
-	}
+      let toDelete = []
 
-	// TODO: if parent behavior, delete all hasMany, and belongsToMany join records
+      for (const t in deps) {
+        const batch = deps[t].rows.rows.map((depInstance) => {
+          return this.doDelete(t, depInstance)
+        })
+        toDelete = toDelete.concat(batch)
+      }
 
-	async doDelete(modelName, instance) {
-		return new Promise(async(resolve, reject) => {
-			let deps, joins;
-			try {
-				deps = await admin.getDependants(modelName, instance);
+      await Promise.all(toDelete)
 
-				// recursively delete dependant rows
+      await instance.destroy()
+    } catch (err) {
+      throw new VError(err, 'error selecting and deleting dependant rows')
+    }
+  }
 
-				let toDelete = [];
+  deleteInstance (modelName, id, cb) {
+    debug('dbHandler.deleteInstance %s %s', modelName, id)
+    this.getModel(modelName).findOne({
+      where: {
+        id: id
+      }
+    }).then((instance) => {
+      this.doDelete(modelName, instance)
+        .then(function () {
+          cb()
+        })
+        .catch(function (err) {
+          cb(new VError(err, 'deleteInstance error'))
+        })
+    }).catch(function (err) {
+      cb(new VError(err, 'deleteInstance error ' + modelName + ' id:' + id + ' not found'))
+    })
+  }
 
-				for (let t in deps) {
-					let batch = deps[t].rows.rows.map((depInstance) => {
-						return this.doDelete(t, depInstance);
-					});
-					toDelete = toDelete.concat(batch);
-				}
+  checkPermission (modelName, user, action, instance) {
+    let permission = 'deny'
 
-				await Promise.all(toDelete);
+    if (!this.getModel(modelName)) {
+      return permission === 'allow'
+    }
 
-				instance.destroy().then(resolve).catch(reject);
-			}
-			catch (err) {
-				reject(new VError(err, "error selecting and deleting dependant rows"))
-			}
-		})
-	}
+    let acl = _.get(this.getModel(modelName), 'options.ADMIN.ACL')
 
-	deleteInstance(modelName, id, cb) {
-		debug('dbHandler.deleteInstance %s %s', modelName, id);
-		this.getModel(modelName).findOne({
-				where: {
-					id: id
-				}
-			})
-			.then((instance) => {
-				this.doDelete(modelName, instance)
-					.then(function () {
-						cb();
-					})
-					.catch(function (err) {
-						cb(new VError(err, 'deleteInstance error'));
-					});
-			})
-			.catch(function (err) {
-				cb(new VError(err, 'deleteInstance error ' + modelName + ' id:' + id + ' not found'));
-			});
-	}
+    if (!acl) {
+      acl = [{
+        permission: 'deny',
+        roles: ['*'],
+        actions: ['*']
+      }]
+    }
 
-	checkPermission(modelName, user, action, instance) {
+    var userRoles = ['*']
 
-		let permission = 'deny';
+    if (user.Roles) {
+      userRoles.push('registered')
+      for (let i = 0; i < user.Roles.length; i++) {
+        userRoles.push(user.Roles[i].description)
+      }
+    } else {
+      userRoles.push('anonymous')
+    }
 
-		if (!this.getModel(modelName)) {
-			return permission === 'allow';
-		}
+    for (const i in acl) {
+      const rule = acl[i]
+      // rule applies to intended action ?
+      if (rule.actions.indexOf(action) !== -1 || rule.actions.indexOf('*') !== -1) {
+        for (const j in rule.roles) {
+          const role = rule.roles[j]
 
-		let acl = _.get(this.getModel(modelName), 'options.ADMIN.ACL');
+          // 'owner' role requires special case check that
+          // userId in the instance is the current user
+          if ((action === 'view' || action === 'update' || action === 'delete') && role === 'owner' && instance) {
+            if (instance.userId === user.id) {
+              permission = rule.permission
+            }
+          } else {
+            // rule applies to user's role ?
+            if (userRoles.indexOf(role) !== -1) {
+              permission = rule.permission
+            }
+          }
+        }
+      }
+    }
 
-		if (!acl) {
-			acl = [{
-				permission: 'deny',
-				roles: ['*'],
-				actions: ['*']
-			}]
-		}
-
-		var userRoles = ['*'];
-
-		if (user.Roles) {
-			userRoles.push('registered');
-			for (let i = 0; i < user.Roles.length; i++) {
-				userRoles.push(user.Roles[i].description);
-			}
-		}
-		else {
-			userRoles.push('anonymous');
-		}
-
-		for (let i in acl) {
-			let rule = acl[i];
-			// rule applies to intended action ?
-			if (rule.actions.indexOf(action) !== -1 || rule.actions.indexOf('*') !== -1) {
-				for (let j in rule.roles) {
-					let role = rule.roles[j];
-
-					// 'owner' role requires special case check that
-					// userId in the instance is the current user
-					if ((action === 'view' || action === 'update' || action === 'delete') && role === 'owner' && instance) {
-						if (instance.userId === user.id) {
-							permission = rule.permission;
-						}
-					}
-					else {
-						// rule applies to user's role ?
-						if (userRoles.indexOf(role) !== -1) {
-							permission = rule.permission;
-						}
-					}
-				}
-			}
-		}
-
-		return permission === 'allow';
-	}
+    return permission === 'allow'
+  }
 }
 
-module.exports = dbHandler;
+module.exports = dbHandler
